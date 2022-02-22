e23d06890e22eebb2cc588eb970c18c1826264cc
Lord Ashes <webtable@gmail.com>
2022-02-22 08:35:09 -0500

    Version 1.0.0
    Initial release

---
+++ Plugin/Patches/Patches.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace LordAshes
+{
+    [BepInDependency(AssetDataPlugin.Guid)]
+    public partial class ExtraGorePlugin : BaseUnityPlugin
+    {
+        static ExtraGorePlugin self = null;
+        static string guidEAR { get; set; }
+        static string guidBodyAura { get; set; }
+        static string guidFilter { get; set; }
+        static string nameTexture { get; set; }
+
+        static float displayTime { get; set; }
+
+        [HarmonyPatch(typeof(Creature), "AttackTargetCreature", new Type[] { typeof(Vector3) })]
+        public static class AttackTargetCreaturePatch
+        {
+            public static bool Prefix(Vector3 impactDir)
+            {
+                CreatureBoardAsset asset;
+                CreaturePresenter.TryGetAsset(LocalClient.SelectedCreatureId, out asset);
+                if (asset != null)
+                {
+                    Debug.Log("Extra Gore Plugin: Applying Gore");
+                    self.StartCoroutine("ApplyGore", new object[] { asset });
+                }
+                else
+                {
+                    Debug.Log("Extra Gore Plugin: Invalid Target");
+                }
+                return true;
+            }
+        }
+
+        private IEnumerator ApplyGore(object[] inputs)
+        {
+            CreatureGuid target = ((CreatureBoardAsset)inputs[0]).Creature.TargetCreature;
+            AssetDataPlugin.SetInfo(CreatureGuid.Empty.ToString(), guidEAR + ".Aura.labloodfilter", guidFilter);
+            AssetDataPlugin.SetInfo(target.ToString(), guidBodyAura, nameTexture, true);
+            yield return new WaitForSeconds(displayTime);
+            Debug.Log("Extra Gore Plugin: Removing Gore");
+            AssetDataPlugin.ClearInfo(CreatureGuid.Empty.ToString(), guidEAR + ".Aura.labloodfilter");
+            AssetDataPlugin.ClearInfo(target.ToString(), guidBodyAura, true);
+        }
+    }
+}
---
+++ Plugin/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency(FileAccessPlugin.Guid)]
+    [BepInDependency(AssetDataPlugin.Guid)]
+    public partial class ExtraGorePlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "Extra Gore Plug-In";              
+        public const string Guid = "org.lordashes.plugins.extragore";
+        public const string Version = "1.0.0.0";                    
+
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Extra Gore Plugin: Active.");
+
+            displayTime = Config.Bind("Settings", "Length In Seconds That Gore Is Displayed", 3.0f).Value;
+            guidEAR = Config.Bind("Settings", "Extra Asset Registration Plugin Guid", "org.lordashes.plugins.extraassetsregistration").Value;
+            guidBodyAura = Config.Bind("Settings", "Body Aura Plugin Guid", "org.lordashes.plugins.bodyaura").Value;
+            guidFilter = Config.Bind("Settings", "Camera Filter Name", "3e736d60-d31a-1a34-177a-867319885255").Value;
+            nameTexture = Config.Bind("Settings", "Body Aura Texture Name", "blood").Value;
+
+            self = this;
+
+            var harmony = new Harmony(Guid);
+            harmony.PatchAll();
+
+            Utility.PostOnMainPage(this.GetType());
+        }
+    }
+}
